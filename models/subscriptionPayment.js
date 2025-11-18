15711ad0edaa138f5f30fe49b5eb01c1eb8936c5
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-11-18 11:34:30 +0530

    Some changes

--- models/subscriptionPayment.js
+++ models/subscriptionPayment.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const SubscriptionPaymentSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', index: true },
-  plan: { type: String, enum: ['Free Trial', 'KONTRONIX', 'SOPAS', 'RTPAS'], required: true },
+  plan: { type: String, enum: ['Free Trial', 'KONTRONIX', 'SOPAS', 'RTPAS'], required: true,default:"Free Trial" },
   amount: { type: Number, default:0 }, // in paise
   currency: { type: String, default: 'INR' },
   razorpayOrderId: { type: String,  },
